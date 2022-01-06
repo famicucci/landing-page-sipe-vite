df2e651f13cdfa2240c98298caca7e07f38c3cff
Francisco Micucci <famicucci@gmail.com>
2022-01-06 17:30:21 -0300

    Add title and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h1 className="text-3xl font-bold">Hello Vite</h1>
-			</header>
+			<div className="flex flex-col justify-center h-screen bg-blue">
+				<h1 className="text-3xl font-bold text-white mb-2">Sip-e</h1>
+				<h4 className=" font-bold text-white">
+					La forma sencilla de gestionar tu negocio
+				</h4>
+				<button className="rounded-md bg-orange-500 font-bold mx-auto mt-4 py-2 px-4 text-white w-fit">
+					Contactar
+				</button>
+			</div>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{react,js,ts,jsx,tsx}'],
 	theme: {
+		colors: {
+			blue: '#3f51b5',
+			white: '#ffffff',
+			orange: { 500: '#ef4444' },
+			slate: {
+				400: '#94a3b8',
+			},
+		},
+		spacing: {
+			1: '8px',
+			2: '12px',
+			3: '16px',
+			4: '24px',
+			5: '32px',
+			6: '48px',
+		},
 		extend: {},
 	},
 	plugins: [],
