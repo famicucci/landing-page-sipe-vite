af736c0b34ae73864a269cb15f154d13f4686f7e
Francisco Micucci <famicucci@gmail.com>
2022-01-06 17:58:31 -0300

    Add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import './App.css';
+import captura from './captura-sipe.jpg';
 
 function App() {
 	return (
-		<div className="App">
-			<div className="flex flex-col justify-center h-screen bg-blue">
-				<h1 className="text-3xl font-bold text-white mb-2">Sip-e</h1>
-				<h4 className=" font-bold text-white">
-					La forma sencilla de gestionar tu negocio
-				</h4>
-				<button className="rounded-md bg-orange-500 font-bold mx-auto mt-4 py-2 px-4 text-white w-fit">
-					Contactar
-				</button>
-			</div>
+		<div className="flex flex-col justify-center h-fit bg-blue p-2">
+			<h1 className="text-3xl text-center font-bold text-white mb-2">Sip-e</h1>
+			<h4 className=" font-bold text-white text-center mb-3">
+				La forma sencilla de gestionar tu negocio
+			</h4>
+			<img src={captura} alt="logo" />
+			<button className="rounded-md bg-orange-500 font-bold mx-auto mt-4 py-2 px-4 text-white w-fit">
+				Contactar
+			</button>
 		</div>
 	);
 }
