3457076812188796c21bcf3ff01ab0cbc4f02341
Francisco Micucci <famicucci@gmail.com>
2022-01-15 23:32:19 -0300

    Create Footer

---
+++ components/sections/footer/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AiFillLinkedin } from 'react-icons/ai';
+
+const Footer = () => {
+	const year = new Date().getFullYear();
+
+	return (
+		<footer className="w-full bg-blue-500 py-14">
+			<div className="mb-10">
+				<p className="text-xl text-white text-center mb-3">
+					Perfil Profesional
+				</p>
+				<a
+					href="https://www.linkedin.com/in/francisco-agustin-micucci/"
+					target="_blank"
+				>
+					<AiFillLinkedin className="text-6xl text-white mx-auto" />
+				</a>
+			</div>
+			<div className="text-sm text-center">
+				<p className="inline mr-2">@ {year}</p>
+				<a
+					className="text-white"
+					href="http://www.franciscomicucci.com/"
+					target="_blank"
+				>
+					franciscomicucci.com
+				</a>
+				<p>Todos los derechos reservados</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from '../components/sections/main';
 import Header from '../components/sections/header';
+import Footer from '../components/sections/footer';
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Main />
+			<Footer />
 		</>
 	);
 }
